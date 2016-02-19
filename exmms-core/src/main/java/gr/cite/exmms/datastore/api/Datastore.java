09bba1d2c47366b6a8ff0b761bc435fb8e191b9b
kapost <apostkonst@gmail.com>
2016-02-19 19:05:04 +0200

    Datastore interface changes

--- exmms-core/src/main/java/gr/cite/exmms/datastore/api/Datastore.java
+++ exmms-core/src/main/java/gr/cite/exmms/datastore/api/Datastore.java
@@ -8,17 +8,19 @@ import gr.cite.exmms.core.Element;
 import gr.cite.exmms.datastore.exceptions.DatastoreException;
 
 public interface Datastore {
-	Element insert(DataElement dataElement) throws DatastoreException;
+	<T extends Element> T insert(T element) throws DatastoreException;
+	
+	<T extends Element> List<T> insert(List<T> dataElements) throws DatastoreException;
 	
 	Element update(Element dataElement) throws DatastoreException;
 
-	void delete(Element dataElement) throws DatastoreException;
+	void remove(Element dataElement) throws DatastoreException;
 	
 	void add(Element dataElement, Collection collection) throws DatastoreException;
 	
-	void remove(Element dataElement, Collection collection) throws DatastoreException;
-
-	List<DataElement> listDataElements();
+	void delete(Element dataElement, Collection collection) throws DatastoreException;
 	
-	List<Collection> listCollections();
+	List<DataElement> listDataElements() throws DatastoreException;
+	
+	List<Collection> listCollections() throws DatastoreException;
 }
